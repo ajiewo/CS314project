6b8c90a081d56ed730c4d8ea20cbfe6f972f55d9
Daniel <dschust@pdx.edu>
2025-03-08 17:32:31 -0800

    added some socket code, still needs debugging

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,28 @@ import dotenv from "dotenv";
 import express, {Router} from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import session from "express-session";
-import MongoStore from "connect-mongo";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import cookieParser from "cookie-parser";
-
-
+import { Server as SocketServer } from "socket.io";
+import http from "http";
 
 //set up constants and variables
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
+const SECRET_KEY = 'mySuperSecreKey'
+const SALT_ROUNDS = 10;
+const userMap = {};
+let server;
+let io;
 app.set('port', PORT);
 
 //middleware
 app.use(cors( {origin: process.env.ORIGIN, credentials: true} ));
 app.use(express.json());
+app.use(cookieParser());
 app.use((req, res, next) => {
       //log basic info for incoming requests
       console.log('new request');
@@ -26,24 +32,23 @@ app.use((req, res, next) => {
       console.log('\tmethod: ', req.method);
       next();
 });
-app.use(cookieParser());
-app.use(session({
-      secret: 'your-secret-key', // Replace with a strong secret in production
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({ mongoUrl: DATABASE_URL, collectionName: "sessions",
-            mongoOptions: {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            }, 
-      }),
-      cookie: { 
-          httpOnly: true,
-          secure: true, // Set to true if using HTTPS
-          sameSite: 'none',
-          maxAge: 1000 * 60 * 60 * 24 // 1 day
+
+// Middleware to verify JWT
+const verifyToken = (req, res, next) => {
+      const token = req.cookies.jwt;
+      if (!token) {
+            return res.status(401).json({ message: 'Not authenticated' });
       }
-  }));
+  
+      jwt.verify(token, SECRET_KEY, (err, payload) => {
+            if (err) {
+                  console.error('Invalid token:', err);
+                  return res.status(403).json({ message: 'Invalid or expired token.' });
+            }
+            req.userId = payload.userId;
+            next();
+      });
+};
 
 
 //define user schema
@@ -51,24 +56,64 @@ const userSchema = new mongoose.Schema({
       email: {type: String, required: true, unique: true},
       password: {type: String, required: true},
       firstName: {type: String},
-      lastName: {type: String}
+      lastName: {type: String},
+      color: {type: String},
 });
 const User = mongoose.model("User", userSchema);
 
+//define contact schema
+const contactSchema = new mongoose.Schema({
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+      email: { type: String, required: true, unique: true },
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+const Contact = mongoose.model("Contact", contactSchema);
+  
+//define message schema
+const messageSchema = new mongoose.Schema({
+      sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+      recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+      content: { type: String, required: true },
+      messageType: { type: String },
+      timestamp: { type: Date, default: Date.now }
+});
+const Message = mongoose.model("Message", messageSchema);
 
 //connect to database, start listening on PORT
 mongoose.connect(DATABASE_URL)
       .then((result) => {
             console.log(`successfully connected to database ${DATABASE_URL}`);
-            app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+            server = http.createServer(app);
+            io = new SocketServer(server, {
+                  cors: {
+                        origin: process.env.ORIGIN,
+                        credentials: true,
+                  },
+                  path: '/socket.io/'
+            });
+            server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+            io.on("connection", (socket) => {
+                  const userId = socket.handshake.query.userId;
+                  console.log(`Client connected: socketID=${socket.id}, userId=${userId}`);
+
+                  userMap[userId] = socket.id;
+
+                  socket.on("disconnect", () => {
+                        console.log(`Client disconnected: socketId=${socket.id}, userId=${userId}`);
+                        delete userMap[userId];
+                  });
+            });
+      
       })
       .catch((err) => console.error("error connecting to database:", err));
+
 
 //user signup API endpoint
 const signup = async (req, res) => {
       try {
             console.log("Received signup request:", req.body);
-
             const {email, password} = req.body;
 
             if (!email || !password) {
@@ -81,8 +126,18 @@ const signup = async (req, res) => {
                   console.error("The email is already in use, sending 409 Conflict");
                   return res.status(409).json({message: "The email is already in use"});
             }
+            const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+            const newUser = new User({ email, password: hashedPassword });
+            const token = jwt.sign({email, userId: newUser.id}, SECRET_KEY, {
+                  expiresIn: '1h',
+            });
+            res.cookie('jwt', token, {
+                  secure: true,
+                  sameSite: 'None',
+                  maxAge: 60 * 60 * 1000, //1 hour
+            });
+            
 
-            const newUser = new User({email, password});
             const savedUser = await newUser.save();
 
             console.log("User saved successfully:", savedUser);
@@ -111,33 +166,26 @@ const login = async (req, res) => {
                   return res.status(404).json({ message: "No user found with the given email" });
             }
 
-            if (user.password !== password) {
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (!isPasswordValid) {
                   console.error("Invalid password, sending 400 Bad Request");
                   return res.status(400).json({ message: "Invalid password" });
             }
 
-            req.session.userId = user._id;
-            req.session.save((err) => {
-                  if (err) {
-                      console.error("Error saving session:", err);
-                      return res.status(500).json({ message: "Error saving session" });
-                  }
-                  console.log("Session saved successfully:", req.session);
-                  res.status(200).json({ message: "Login successful." });
-              });
-            console.log("Session created:", req.session);
-    
+            //valid login, create token
+            const token = jwt.sign({email, userId: user.id}, SECRET_KEY, {
+                  expiresIn: '1h',
+            });
+
+            res.cookie('jwt', token, {
+                  secure: true,
+                  sameSite: 'None',
+                  maxAge: 60 * 60 * 1000,
+            });
+
             console.log(`Login successful for ${email}`);
 
-            /*
-            res.cookie('sessionID', req.sessionID, {
-                  httpOnly: true,
-                  secure: true,
-                  sameSite: 'none',
-            });
-            */
-
-            res.status(200).json({ message: "Login successful." });
+            res.status(200).json({ message: 'Login successful', token });
       }
       catch (error) {
             console.error("Login error:", error);
@@ -149,20 +197,9 @@ const login = async (req, res) => {
 
 //user logout API endpoint
 const logout = (req, res) => {
-      /*
       try {
+            res.clearCookie('jwt', { secure: true, sameSite: 'None' });
             res.status(200).json({ message: "Logout successful" });
-      }
-            */
-      try {
-            req.session.destroy((err) => {
-                if (err) {
-                    console.error("Error destroying session:", err);
-                    return res.status(500).json({ message: "Internal Server Error" });
-                }
-                res.clearCookie('connect.sid'); // Clear the session cookie
-                res.status(200).json({ message: "Logout successful" });
-            });
       }
       catch (error) {
             console.error("Logout error:", error);
@@ -170,102 +207,174 @@ const logout = (req, res) => {
       }
 };
 
-//get user info API endpoint
+// Protected route to get user info
 const userinfo = async (req, res) => {
       try {
-            console.log("Session data:", req.session);
-            if (!req.session.userId) {
-                console.error("No user ID in session");
-                return res.status(404).json({ message: "No user ID in session" });
+            const currentUser = await User.findById(req.userId);
+            if (!currentUser) {
+                  return res.status(404).json({ message: 'User not found' });
             }
-
-            const { email } = req.query;
-
-            console.log("Received email from header:", email);
-    
-            console.log("Request query:", req.query); // Check the entire query object
-            console.log("Request body:", req.body); // Check the entire query object
-            console.log("Request headers:", req.headers); // Check the entire query object
-            //const user = await User.findOne({ email });
-            const user = await User.findByID(req.session.userID);
-            if (!email || !user) {
-                console.error("User ID not in token or user doesn't exist in the database, sending 404 Not Found");
-                return res.status(404).json({ message: "User ID not in token or user doesn't exist in the database" });
-            }
-    
-            res.status(200).json({ email: user.email, firstName: user.firstName, lastName: user.lastName });
-      }
-      catch (error) {
-            console.error("User info error:", error);
-            res.status(500).json({ message: "Unexpected Server Error" });
+            res.status(200).json({
+                  id: currentUser.id,
+                  email: currentUser.email,
+                  password: currentUser.password,
+            });
+      } catch (error) {
+            console.error('Error fetching user info:', error);
+            res.status(500).json({ message: 'Server error' });
       }
 };
 
 //update user profile API endpoint
-//needs request body with firstname, lastname
 const updateprofile = async (req, res) => {
       try {
-            const { email, firstName, lastName, color } = req.body;
-            if (!email || !firstName || !lastName) {
-                return res.status(400).json({ message: "Missing required fields" });
+            const { color, firstName, lastName } = req.body;
+            const currentUser = await User.findById(req.userId);
+            const email = currentUser.email;
+            if (!color || !firstName || !lastName) {
+                  return res.status(400).json({ message: "Missing required fields" });
             }
             const user = await User.findOneAndUpdate(
-                { email },
-                { firstName, lastName, color },
-                { new: true }
+                  { email },
+                  { firstName, lastName, color },
+                  { new: true }
             );
             if (!user) {
-                return res.status(404).json({ message: "User not found" });
+                  return res.status(404).json({ message: "User not found" });
             }
+            console.log("Successfully updated profile for: ", user);
             res.status(200).json({ message: "Profile successfully updated" });
-      } catch (error) {
+      } 
+      catch (error) {
+            console.error("Error updating profile: ", error);
             res.status(500).json({ message: "Server or database issue" });
       }
 };
 
-//search contacts API endpoint
-app.post("/api/contacts/search", async (req, res) => {
-      try {
-      }
-      catch {
-      }
-});
-
-//get contacts API endpoint
-app.get("/api/contacts/get-contacts-for-list", async (req, res) => {
-      try {
-      }
-      catch {
-      }
-});
-
-//delete messages API endpoint
-app.delete("/api/contacts/delete-dm/:dmId", async (req, res) => {
-      try {
-      }
-      catch {
-      }
-});
-
-//get messages API endpoint
-app.post("/api/messages/get-messages", async (req, res) => {
-      try {
-      }
-      catch {
-      }
-});
-
-//define router and endpoints
+//define /api/auth router and endpoints
 const authRoutes = Router();
 app.use('/api/auth', authRoutes);
-
-
-//POST /api/auth/signup
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
 authRoutes.post("/logout", logout);
-authRoutes.get("/userinfo", userinfo);
-authRoutes.post("/update-profile", updateprofile);
+authRoutes.get("/userinfo", verifyToken, userinfo);
+authRoutes.post("/update-profile", verifyToken, updateprofile);
+
+
+//search contacts API endpoint
+const search = async (req, res) => {
+      try {
+            const { query } = req.body;
+            if (!query) {
+                  return res.status(400).json({ message: "Search query is required" });
+            }
+            const contacts = await Contact.find({
+                  $or: [
+                        { firstName: { $regex: query, $options: "i" } },
+                        { lastName: { $regex: query, $options: "i" } },
+                        { email: { $regex: query, $options: "i" } }
+                  ]
+            });
+            res.status(200).json({ contacts });
+      }
+      catch (error) {
+            console.error("Error searching contacts:", error);
+            res.status(500).json({ message: "Server or database issue" });
+      }
+};
+
+//get contacts API endpoint
+const getContacts = async (req, res) => {
+      try {
+            const contacts = await Contact.find({}, "firstName lastName email");
+            res.status(200).json({ contacts });
+      }
+      catch (error) {
+            console.error("Error fetching contacts:", error);
+            res.status(500).json({ message: "Server or database issue" });
+      }
+};
+
+//placeholder for unimplemented endpoints
+const empty = (req, res) => {
+      res.status(200).json({ message: "not implemented yet" })
+};
+
+//define /api/contacts router and endpoints
+const contactRoutes = Router();
+app.use("/api/contacts", contactRoutes);
+contactRoutes.post("/search", verifyToken, search);
+contactRoutes.get("/get-contacts-for-list", verifyToken, getContacts);
+contactRoutes.get("/all-contacts", verifyToken, empty); //ts line
+
+//define /api/channel router and endpoints
+const channelRoutes = Router();
+app.use("/api/channel", channelRoutes);
+channelRoutes.get("/get-user-channels", verifyToken, empty); //ts line
+
+//delete messages API endpoint
+const deleteDm = async (req, res) => {
+      try {
+            const { dmId } = req.params;
+            const deletedMessage = await Message.findByIdAndDelete(dmId);
+            if (!deletedMessage) {
+                  return res.status(400).json({ message: "Missing or invalid dmId" });
+            }
+            res.status(200).json({ message: "DM deleted successfully" });
+      } 
+      catch (error) {
+            console.error("Error deleting message:", error);
+            res.status(500).json({ message: "Server or database issue" });
+      }
+};
+
+//get messages API endpoint
+const getMessages = async (req, res) => {
+      try {
+            const { contactId } = req.body;
+            if (!contactId) {
+                  return res.status(400).json({ message: "Missing one or both user IDs" });
+            }
+            const messages = await Message.find({
+                  $or: [
+                        { sender: req.userId, receiver: contactId },
+                        { sender: contactId, receiver: req.userId }
+                  ]
+            }).sort({ createdAt: 1 });
+            res.status(200).json({ messages });
+      } 
+      catch (error) {
+            console.error("Error fetching messages:", error);
+            res.status(500).json({ message: "Server or database issue" });
+      }
+};
+
+
+const chatHandler = (req, res) => {
+      console.log('chat request body: ', req.body);
+      const { sender, recipient, content, messageType } = req.body;
+      if (!sender || !content) {
+            console.log("Missing sender or text");
+            return res.status(400).json({ error: "Missing sender or text" });
+      }
+
+      const newMessage = new Message ({ sender, recipient, content, messageType });
+      if (userMap[sender]) {
+            io.to(userMap[sender]).emit("newMessage", newMessage);
+      }
+      if (userMap[recipient]) {
+            io.to(userMap[recipient]).emit("newMessage", newMessage);
+      }
+      console.log(`Emitted ${newMessage}`);
+      newMessage.save();
+
+      return res.status(201).json({ success: true, message: newMessage });
+};
+
+//define /api/messages router and endpoints
+app.post("/api/messages", verifyToken, chatHandler);
+app.delete("/api/messages/delete-dm/:dmId", verifyToken, deleteDm);
+app.post("/api/messages/get-messages", verifyToken, getMessages);
 
 
 //debugging fallback to list unmatched routes
